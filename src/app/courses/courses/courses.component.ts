3c56d63361e31f51977cfaa1fd11fd698a818e7f
wagnerk78 <wagnerkroiss@gmail.com>
2023-09-14 11:15:00 -0300

    14th September

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { Course } from '../models/course';
 import { Observable, catchError, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -13,11 +14,13 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 export class CoursesComponent {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   // courseService: CoursesService
 constructor(private courseService: CoursesService,
-  public dialog: MatDialog
+  public dialog: MatDialog,
+  private router: Router,
+  private route: ActivatedRoute
 
   ) {
   // this.courseService = new CoursesService();
@@ -37,6 +40,14 @@ onError(errorMsg: string) {
     data: errorMsg
   });
 }
+
+onAdd() {
+ this.router.navigate(['new'], {relativeTo: this.route});
+}
+
+
+
+
 }
 
 
