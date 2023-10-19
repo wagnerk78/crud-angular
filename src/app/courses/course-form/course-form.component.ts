5f7ebe4748fa35dcc8be4f9744b21bd1f2db033d
wagnerk78 <wagnerkroiss@gmail.com>
2023-10-19 20:05:16 -0300

    19th October

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Course } from '../models/course';
+
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+
+form: FormGroup;
+
+
+// @Input()
+// get value(): Course | null {
+//   let n = this.form.value;
+// return n
+// }
+// set value(c: Course | null) {
+//   this.form.setValue({id: c?._id, name: c?.name, category: c?.category })
+// }
+
+
+constructor(private fb: FormBuilder) {
+this.form = fb.group({
+ 'name': 'null',
+ 'category': 'null'
+
+})
+}
+
+ngOnInit(): void {
+
+}
 
 }
--- src/app/courses/models/course.ts
+++ src/app/courses/models/course.ts
@@ -3,4 +3,6 @@ export interface Course {
   name: string;
   category: string;
 
+
+
 }
